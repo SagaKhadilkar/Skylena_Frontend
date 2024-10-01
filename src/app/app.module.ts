f855b04059b629456eb4728b2e7ebed57238460d
SagarKhadilkar <sagaruk143@gmail.com>
2024-10-01 16:26:48 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { RegisterComponent } from './register/register.component';
+import { RouterComponent } from './router/router.component';
+import { Service1Component } from './service1/service1.component';
+import { Service2Component } from './service2/service2.component';
+import { Service3Component } from './service3/service3.component';
+import { Service4Component } from './service4/service4.component';
+import { Service5Component } from './service5/service5.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    RegisterComponent,
+    RouterComponent,
+    Service1Component,
+    Service2Component,
+    Service3Component,
+    Service4Component,
+    Service5Component,
+    AboutUsComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
@@ -15,4 +35,6 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule { 
+  imagePath = 'assets/Images/bg.1.png';
+}
